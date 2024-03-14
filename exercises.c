fc87c05390efa059534c6d9dd34faa9b8d310a59
Your Name <you@example.com>
2024-03-14 21:15:01 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -45,7 +45,11 @@ newsize apunta a una dirección válida que no ha sido inicializada con nigún
 valor específico. *newsize debe almacenar el tamaño del nuevo arreglo que se
 retorna.
 */
-int *filterEvenNumbers(int arr[], int size, int *newSize) { return NULL; }
+int *filterEvenNumbers(int arr[], int size, int *newSize) 
+{ 
+  
+  return 3; 
+}
 
 /*
 Ejercicio 4: Fusión de dos Arreglos Ordenados
